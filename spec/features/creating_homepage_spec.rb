d733847c1df044b8bbd1a4e703dd9cb99c56c523
Noriyo Akita <noriyo.akita@gmail.com>
2016-02-05 05:33:04 +0000

    Modify creating_homepage_spec using feature(Capybara's keyword)

--- spec/features/creating_homepage_spec.rb
+++ spec/features/creating_homepage_spec.rb
@@ -1,9 +1,10 @@
 RSpec.feature "Creating Home Page", type: :feature do
-  scenario "visit home page" do
-    visit "/"
-
-    expect(page).to have_link "Athletes Den"
-    expect(page).to have_link "Home"
-    expect(page).to have_content "Workout Lounge!"
+  feature "when the user visits home page" do
+    before { visit "/" }
+    scenario "the user can see the links" do
+      expect(page).to have_link "Athletes Den"
+      expect(page).to have_link "Home"
+      expect(page).to have_content "Workout Lounge!"
+    end
   end
 end
