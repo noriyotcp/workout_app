3f9579e51e2dd883da268f5d701b911d76952b06
Noriyo Akita <noriyo.akita@gmail.com>
2016-02-08 07:57:04 +0000

    Add #full_name method to User and its spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,11 @@ RSpec.describe User, type: :model do
     it { is_expected.to validate_presence_of :first_name }
     it { is_expected.to validate_presence_of :last_name }
   end
+
+  describe "#full_name" do
+    let(:user) { create(:user) }
+    subject { user.full_name }
+
+    it { is_expected.to eq "#{user.first_name} #{user.last_name}" }
+  end
 end
