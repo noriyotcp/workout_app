335de44462790d8a9ad769dce4842478eb51ce18
Noriyo Akita <noriyo.akita@gmail.com>
2016-02-19 12:20:56 +0000

    Add a scenario when it fails to create a new exercise

--- spec/features/creating_exercise_spec.rb
+++ spec/features/creating_exercise_spec.rb
@@ -1,5 +1,3 @@
-require 'rails_helper'
-
 RSpec.feature "CreatingExercise", type: :feature do
   given(:user) { create(:user) }
 
@@ -20,5 +18,22 @@ RSpec.feature "CreatingExercise", type: :feature do
     exercise = Exercise.last
 
     expect(current_path).to eq user_exercise_path(user, exercise)
+  end
+
+  scenario "fail to create an exercise" do
+    visit "/"
+    click_link "My Lounge"
+    click_link "New Workout"
+    expect(page).to have_content "Back"
+
+    fill_in "Duration", with: nil
+    fill_in "Workout Details", with: ""
+    fill_in "Activity date", with: ""
+    click_button "Create Exercise"
+    expect(page).to have_content "Exercise has not been created"
+    expect(page).to have_content "Duration in min can't be blank"
+    expect(page).to have_content "Duration in min is not a number"
+    expect(page).to have_content "Workout can't be blank"
+    expect(page).to have_content "Workout date can't be blank"
   end
 end
