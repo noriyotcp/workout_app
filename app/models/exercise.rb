c3eda93f4ef48cf404724c7fecfe90eb1ff0ce9e
Noriyo Akita <noriyo.akita@gmail.com>
2016-02-19 12:13:10 +0000

    Add validations to Exercise model

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -19,4 +19,7 @@ class Exercise < ActiveRecord::Base
   belongs_to :user
 
   validates :user, presence: true
+  validates :duration_in_min, presence: true, numericality: { only_integer: true }
+  validates :workout, presence: true
+  validates :workout_date, presence: true
 end
