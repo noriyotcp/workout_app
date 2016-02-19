5c8a36b997c68c3a5018fca7a8cb2c99eb59e7a1
Noriyo Akita <noriyo.akita@gmail.com>
2016-02-19 01:42:42 +0000

    Add show action and set_exercise to ExercisesController

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,5 +1,11 @@
 class ExercisesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_exercise, only: [:show]
+
   def index
+  end
+
+  def show
   end
 
   def new
@@ -22,5 +28,9 @@ class ExercisesController < ApplicationController
 
     def exercise_params
       params.require(:exercise).permit(:duration_in_min, :workout, :workout_date, :user)
+    end
+
+    def set_exercise
+      @exercise = current_user.exercises.find(params[:id])
     end
 end
