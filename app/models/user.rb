bdbc99834c073cf2c4ef1d8e2a44cb379fc8c6cf
Noriyo Akita <noriyo.akita@gmail.com>
2016-02-24 13:17:04 +0000

    Add pagination to dashboard/index

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,6 +36,7 @@ class User < ActiveRecord::Base
 
   validates :first_name, presence: true
   validates :last_name, presence: true
+  self.per_page = 5
 
   def full_name
     "#{first_name} #{last_name}"
