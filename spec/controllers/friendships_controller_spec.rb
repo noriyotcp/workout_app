0ca882a359e8d276b786ff62c2183f4ae0a0bc35
Noriyo Akita <noriyo.akita@gmail.com>
2016-02-25 12:51:27 +0000

    Add examples to unfollow friends to friendships_controller_spec

--- spec/controllers/friendships_controller_spec.rb
+++ spec/controllers/friendships_controller_spec.rb
@@ -1,42 +1,47 @@
 RSpec.describe FriendshipsController, type: :controller do
-  let(:john) { create(:user) }
-  let(:peter) { create(:user) }
-  let(:friendships) { Friendship.create(user: john, friend: peter) }
+  let(:user) { create(:user) }
+  let(:friend) { create(:user) }
+  let!(:friendship) { Friendship.create(user: user, friend: friend) }
+
+  before { sign_in user }
 
   describe "GET #show" do
-    before { sign_in(john) }
-
     it "returns http success" do
-      get :show, { id: friendships }
+      get :show, { id: friendship }
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "POST #create" do
-    before { sign_in(john) }
-
     it "returns status 302" do
-      post :create, { friend_id: peter }
+      post :create, { friend_id: friend }
+      expect(response).to redirect_to root_path
       expect(response).to have_http_status(302)
     end
 
     it "creates a new friend" do
         expect {
-          post :create, { friend_id: peter }
-        }.to change(john.friendships, :count).by(1)
+          post :create, { friend_id: friend }
+        }.to change(user.friendships, :count).by(1)
       end
 
       it "redirects to the root" do
-        post :create, { friend_id: peter }
+        post :create, { friend_id: friend }
         expect(response).to redirect_to root_path
       end
   end
 
   describe "GET #destroy" do
-    it "returns http success" do
-      get :destroy
-      expect(response).to have_http_status(:success)
+    it "destroys the requested friendship" do
+      expect {
+        delete :destroy, { id: friendship }
+      }.to change(Friendship, :count).by(-1)
+    end
+
+    it "returns status 302" do
+      get :destroy, { id: friendship }
+      expect(response).to redirect_to user_exercises_path(user)
+      expect(response).to have_http_status(302)
     end
   end
-
 end
